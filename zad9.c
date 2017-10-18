88ec4cf290f6123b7adb953b48a13781affe36d3
Bobolowicz <bobolowicz@gmail.com>
2017-10-18 16:45:27 +0200

    kostka

---
+++ zad9.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib>
+
+int main(void) {
+int Yakko,Wakko,Dot;
+
+	Yakko = (int)(rand() / (RAND_MAX + 1.0)
+
+printf("Szansa na wyjazd Dot do Transylwani wynosi");
+}
